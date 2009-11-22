98f87447695811e1c0d0c3480481adfd7519ac5e
johnsheehan <johnsheehan@gmail.com>
2009-11-22 17:57:21 -0600

    License regions

--- RestSharp/Deserializers/IDeserializer.cs
+++ RestSharp/Deserializers/IDeserializer.cs
@@ -1,4 +1,5 @@
-﻿//   Copyright 2009 John Sheehan
+﻿#region License
+//   Copyright 2009 John Sheehan
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -10,7 +11,8 @@
 //   distributed under the License is distributed on an "AS IS" BASIS,
 //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //   See the License for the specific language governing permissions and
-//   limitations under the License.
+//   limitations under the License. 
+#endregion
 
 namespace RestSharp.Deserializers
 {
