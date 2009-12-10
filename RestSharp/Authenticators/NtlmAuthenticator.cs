f565e9d1f7d435ed075b45f686520da8bbe39bd7
johnsheehan <johnsheehan@gmail.com>
2009-12-10 00:40:11 -0600

    Added mstum's NTLM authenticator. Thanks!

---
+++ RestSharp/Authenticators/NtlmAuthenticator.cs
@@ -0,0 +1,28 @@
+﻿#region License
+//   Copyright 2009 John Sheehan
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License. 
+#endregion
+
+namespace RestSharp.Authenticators
+{
+	/// <summary>
+	/// Tries to Authenticate with the credentials of the currently logged in user
+	/// </summary>
+	public class NTLMAuthenticator : IAuthenticator
+	{
+		public void Authenticate(RestRequest request) {
+			request.Credentials = System.Net.CredentialCache.DefaultCredentials;
+		}
+	}
+}
