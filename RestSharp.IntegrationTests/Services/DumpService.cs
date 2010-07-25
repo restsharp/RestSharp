323c7e486b247227f6c41ffb13c8023fc3727ce0
johnsheehan <johnsheehan@gmail.com>
2010-07-25 13:44:31 -0600

    Misc test cruft/infrastructure for future test scenarios

---
+++ RestSharp.IntegrationTests/Services/DumpService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kayak.Framework;
+
+namespace RestSharp.IntegrationTests.Services
+{
+	public class DumpService : KayakService
+	{
+		[Path("/Dump"), Verb("PUT")]
+		public void Dump()
+		{
+			Response.Write(Request.ContentLength);
+		}
+	}
+}
--- RestSharp.IntegrationTests/TestBase.cs
+++ RestSharp.IntegrationTests/TestBase.cs
@@ -21,5 +21,6 @@ namespace RestSharp.IntegrationTests
 			_server.Stop();
 		}
 
+		
 	}
 }
