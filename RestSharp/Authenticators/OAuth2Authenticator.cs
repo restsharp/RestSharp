e167260d08b5d7ad1929dd4f6a70dabf97d677ac
johnsheehan <johnsheehan@gmail.com>
2010-08-08 11:57:44 -0600

    OAuth2Authenicator cleanup (down with regions)

--- RestSharp/Authenticators/OAuth2Authenticator.cs
+++ RestSharp/Authenticators/OAuth2Authenticator.cs
@@ -1,56 +1,48 @@
 namespace RestSharp
 {
-    /// <summary>
-    /// Base class for OAuth 2 Authenticators.
-    /// </summary>
-    /// <remarks>
-    /// Since there are many ways to authenticate in OAuth2,
-    /// this is used as a base class to differentiate between 
-    /// other authenticators.
-    /// 
-    /// Any other OAuth2 authenticators must derive from this
-    /// abstract class.
-    /// </remarks>
-    public abstract class OAuth2Authenticator : IAuthenticator
-    {
-        #region Implementation of IAuthenticator
+	/// <summary>
+	/// Base class for OAuth 2 Authenticators.
+	/// </summary>
+	/// <remarks>
+	/// Since there are many ways to authenticate in OAuth2,
+	/// this is used as a base class to differentiate between 
+	/// other authenticators.
+	/// 
+	/// Any other OAuth2 authenticators must derive from this
+	/// abstract class.
+	/// </remarks>
+	public abstract class OAuth2Authenticator : IAuthenticator
+	{
+		public abstract void Authenticate(RestRequest request);
+	}
 
-        public abstract void Authenticate(RestRequest request);
+	/// <summary>
+	/// The OAuth 2 authenticator using URI query parameter.
+	/// </summary>
+	/// <remarks>
+	/// Based on http://tools.ietf.org/html/draft-ietf-oauth-v2-10#section-5.1.2
+	/// </remarks>
+	public class OAuth2UriQueryParameterAuthenticator : OAuth2Authenticator
+	{
+		/// <summary>
+		/// Access token to be used when authenticating.
+		/// </summary>
+		private readonly string _accessToken;
 
-        #endregion
-    }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="OAuth2UriQueryParameterAuthenticator"/> class.
+		/// </summary>
+		/// <param name="accessToken">
+		/// The access token.
+		/// </param>
+		public OAuth2UriQueryParameterAuthenticator(string accessToken)
+		{
+			_accessToken = accessToken;
+		}
 
-    /// <summary>
-    /// The OAuth 2 authenticator using URI query paramater.
-    /// </summary>
-    /// <remarks>
-    /// Based on http://tools.ietf.org/html/draft-ietf-oauth-v2-10#section-5.1.2
-    /// </remarks>
-    public class OAuth2UriQueryParamaterAuthenticator : OAuth2Authenticator
-    {
-        /// <summary>
-        /// Access token to be used when authenticating.
-        /// </summary>
-        private readonly string _accessToken;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="OAuth2UriQueryParamaterAuthenticator"/> class.
-        /// </summary>
-        /// <param name="accessToken">
-        /// The access token.
-        /// </param>
-        public OAuth2UriQueryParamaterAuthenticator(string accessToken)
-        {
-            _accessToken = accessToken;
-        }
-
-        #region Overrides of OAuth2Authenticator
-
-        public override void Authenticate(RestRequest request)
-        {
-            request.AddParameter("oauth_token", _accessToken, ParameterType.GetOrPost);
-        }
-
-        #endregion
-    }
+		public override void Authenticate(RestRequest request)
+		{
+			request.AddParameter("oauth_token", _accessToken, ParameterType.GetOrPost);
+		}
+	}
 }
