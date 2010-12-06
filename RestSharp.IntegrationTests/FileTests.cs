ff5a2ef906bd9529e3e9b026eb9537dd0090dc73
Jon Fuller <fullerjc@gmail.com>
2010-12-06 11:32:03 -0500

    content length shouldn't be set when multipart form data (files) are part of the request

--- RestSharp.IntegrationTests/FileTests.cs
+++ RestSharp.IntegrationTests/FileTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using Xunit;
 
@@ -17,6 +18,19 @@ namespace RestSharp.IntegrationTests
 
 			var expected = File.ReadAllBytes(Environment.CurrentDirectory + "\\Assets\\Koala.jpg");
 			Assert.Equal(expected, response);
+		}
+
+		[Fact]
+		public void Handles_MultipartFormData_With_File(){
+			var client = new RestClient(BaseUrl);
+			var request = new RestRequest("upload"){Method = Method.POST}; // non-existent URL
+
+			request.AddFile("Assets\\Koala.jpg");
+			request.AddParameter("parameter", "parameter value");
+
+			var response = client.Execute(request);
+
+			Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
 		}
 	}
 }
