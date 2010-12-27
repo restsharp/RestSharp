893837d73092a55d6808c804ae7cab2291bb0c61
johnsheehan <johnsheehan@gmail.com>
2010-12-26 22:17:51 -0600

    Some oAuth and URL handling tweaks

---
+++ RestSharp.IntegrationTests/oAuth1Tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.Net;
+using RestSharp.Contrib;
+using RestSharp.Authenticators;
+using System.Diagnostics;
+
+namespace RestSharp.IntegrationTests
+{
+	public class oAuth1Tests
+	{
+		[Fact]
+		public void Can_Authenticate_With_OAuth()
+		{
+			const string consumerKey = "fKGg2R7jINCYgGLMerhrg";
+			const string consumerSecret = "DIGd6nBx3jchwwbMClmHshh8Ky24lq3pNJragxTg";
+
+			var baseUrl = "https://api.twitter.com";
+			var client = new RestClient(baseUrl);
+			client.Authenticator = OAuth1Authenticator.ForRequestToken(consumerKey, consumerSecret);
+			var request = new RestRequest("oauth/request_token", Method.POST);
+			var response = client.Execute(request);
+
+			Assert.NotNull(response);
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+			var qs = HttpUtility.ParseQueryString(response.Content);
+			var oauth_token = qs["oauth_token"];
+			var oauth_token_secret = qs["oauth_token_secret"];
+			Assert.NotNull(oauth_token);
+			Assert.NotNull(oauth_token_secret);
+
+			request = new RestRequest("oauth/authorize");
+			request.AddParameter("oauth_token", oauth_token);
+			var url = client.BuildUri(request).ToString();
+			Process.Start(url);
+
+			var verifier = "123456"; // <-- Breakpoint here (set verifier in debugger)
+			request = new RestRequest("oauth/access_token");
+			client.Authenticator = OAuth1Authenticator.ForAccessToken(
+				consumerKey, consumerSecret, oauth_token, oauth_token_secret, verifier
+			);
+			response = client.Execute(request);
+
+			Assert.NotNull(response);
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+			qs = HttpUtility.ParseQueryString(response.Content);
+			oauth_token = qs["oauth_token"];
+			oauth_token_secret = qs["oauth_token_secret"];
+			Assert.NotNull(oauth_token);
+			Assert.NotNull(oauth_token_secret);
+
+			request = new RestRequest("account/verify_credentials.xml");
+			client.Authenticator = OAuth1Authenticator.ForProtectedResource(
+				consumerKey, consumerSecret, oauth_token, oauth_token_secret
+			);
+
+			response = client.Execute(request);
+
+			Assert.NotNull(response);
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+		}
+
+	}
+}
