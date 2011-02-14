2f95bd8cae94e12f2d8dfc69079e99472a5235c5
johnsheehan <johnsheehan@gmail.com>
2011-02-13 23:50:15 -0700

    tabify

--- RestSharp.IntegrationTests/oAuth1Tests.cs
+++ RestSharp.IntegrationTests/oAuth1Tests.cs
@@ -63,90 +63,90 @@ namespace RestSharp.IntegrationTests
 			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 		}
 
-	    #region Netflix test classes
+		#region Netflix test classes
 
-	    [XmlRoot("queue")]
-	    private class Queue
-	    {
-	        [XmlElement("etag")]
-	        public string Etag { get; set; }
+		[XmlRoot("queue")]
+		private class Queue
+		{
+			[XmlElement("etag")]
+			public string Etag { get; set; }
 
-	        public List<QueueItem> Items { get; set; }
-	    }
+			public List<QueueItem> Items { get; set; }
+		}
 
-	    [XmlRoot("queue_item")]
-	    private class QueueItem
-	    {
-	        [XmlElement("id")]
-	        public string ID { get; set; }
+		[XmlRoot("queue_item")]
+		private class QueueItem
+		{
+			[XmlElement("id")]
+			public string ID { get; set; }
 
-	        [XmlElement("position")]
-	        public int Position { get; set; }
-	    }
+			[XmlElement("position")]
+			public int Position { get; set; }
+		}
 
-	    #endregion
+		#endregion
 
-        //[Fact]
-        public void Can_Authenticate_Netflix_With_OAuth()
-        {
-            const string consumerKey = "";
-            const string consumerSecret = "";
+		//[Fact]
+		public void Can_Authenticate_Netflix_With_OAuth()
+		{
+			const string consumerKey = "";
+			const string consumerSecret = "";
 
-            var baseUrl = "http://api.netflix.com";
-            var client = new RestClient(baseUrl);
-            client.Authenticator = OAuth1Authenticator.ForRequestToken(consumerKey, consumerSecret);
-            var request = new RestRequest("oauth/request_token");
-            var response = client.Execute(request);
+			var baseUrl = "http://api.netflix.com";
+			var client = new RestClient(baseUrl);
+			client.Authenticator = OAuth1Authenticator.ForRequestToken(consumerKey, consumerSecret);
+			var request = new RestRequest("oauth/request_token");
+			var response = client.Execute(request);
 
-            Assert.NotNull(response);
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+			Assert.NotNull(response);
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            var qs = HttpUtility.ParseQueryString(response.Content);
-            var oauth_token = qs["oauth_token"];
-            var oauth_token_secret = qs["oauth_token_secret"];
-            var applicationName = qs["application_name"];
-            Assert.NotNull(oauth_token);
-            Assert.NotNull(oauth_token_secret);
-            Assert.NotNull(applicationName);
+			var qs = HttpUtility.ParseQueryString(response.Content);
+			var oauth_token = qs["oauth_token"];
+			var oauth_token_secret = qs["oauth_token_secret"];
+			var applicationName = qs["application_name"];
+			Assert.NotNull(oauth_token);
+			Assert.NotNull(oauth_token_secret);
+			Assert.NotNull(applicationName);
 
-            var baseSslUrl = "https://api-user.netflix.com";
-            var sslClient = new RestClient(baseSslUrl);
-            request = new RestRequest("oauth/login");
-            request.AddParameter("oauth_token", oauth_token);
-            request.AddParameter("oauth_consumer_key", consumerKey);
-            request.AddParameter("application_name", applicationName);
-            var url = sslClient.BuildUri(request).ToString();
-            
-            Process.Start(url);
+			var baseSslUrl = "https://api-user.netflix.com";
+			var sslClient = new RestClient(baseSslUrl);
+			request = new RestRequest("oauth/login");
+			request.AddParameter("oauth_token", oauth_token);
+			request.AddParameter("oauth_consumer_key", consumerKey);
+			request.AddParameter("application_name", applicationName);
+			var url = sslClient.BuildUri(request).ToString();
+			
+			Process.Start(url);
 
-            request = new RestRequest("oauth/access_token"); // <-- Breakpoint here, login to netflix
-            client.Authenticator = OAuth1Authenticator.ForAccessToken(
-                consumerKey, consumerSecret, oauth_token, oauth_token_secret
-            );
-            response = client.Execute(request);
+			request = new RestRequest("oauth/access_token"); // <-- Breakpoint here, login to netflix
+			client.Authenticator = OAuth1Authenticator.ForAccessToken(
+				consumerKey, consumerSecret, oauth_token, oauth_token_secret
+			);
+			response = client.Execute(request);
 
-            Assert.NotNull(response);
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+			Assert.NotNull(response);
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            qs = HttpUtility.ParseQueryString(response.Content);
-            oauth_token = qs["oauth_token"];
-            oauth_token_secret = qs["oauth_token_secret"];
-            var user_id = qs["user_id"];
-            Assert.NotNull(oauth_token);
-            Assert.NotNull(oauth_token_secret);
-            Assert.NotNull(user_id);
+			qs = HttpUtility.ParseQueryString(response.Content);
+			oauth_token = qs["oauth_token"];
+			oauth_token_secret = qs["oauth_token_secret"];
+			var user_id = qs["user_id"];
+			Assert.NotNull(oauth_token);
+			Assert.NotNull(oauth_token_secret);
+			Assert.NotNull(user_id);
 
-            client.Authenticator = OAuth1Authenticator.ForProtectedResource(consumerKey, consumerSecret, oauth_token, oauth_token_secret);
-            request = new RestRequest("users/{user_id}/queues/disc");
-            request.AddUrlSegment("user_id", user_id);
-            request.AddParameter("max_results", "2");
-            var queueResponse = client.Execute<Queue>(request);
+			client.Authenticator = OAuth1Authenticator.ForProtectedResource(consumerKey, consumerSecret, oauth_token, oauth_token_secret);
+			request = new RestRequest("users/{user_id}/queues/disc");
+			request.AddUrlSegment("user_id", user_id);
+			request.AddParameter("max_results", "2");
+			var queueResponse = client.Execute<Queue>(request);
 
-            Assert.NotNull(queueResponse);
-            Assert.Equal(HttpStatusCode.OK, queueResponse.StatusCode);
+			Assert.NotNull(queueResponse);
+			Assert.Equal(HttpStatusCode.OK, queueResponse.StatusCode);
 
-            Assert.NotNull(queueResponse.Data);
-            Assert.Equal(2, queueResponse.Data.Items.Count);
-        }
+			Assert.NotNull(queueResponse.Data);
+			Assert.Equal(2, queueResponse.Data.Items.Count);
+		}
 	}
 }
