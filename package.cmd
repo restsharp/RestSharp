0e600fb61b85f9e68f3bf1693250d7bfc23ad5a2
johnsheehan <johnsheehan@gmail.com>
2011-05-19 00:39:54 -0600

    adding .net 4 project to make nuget work better with json.netreference

--- package.cmd
+++ package.cmd
@@ -4,6 +4,8 @@ if not exist Download\package mkdir Download\package
 if not exist Download\package\lib mkdir Download\package\lib
 if not exist Download\package\lib\net35 mkdir Download\package\lib\net35
 if not exist Download\package\lib\net35-client mkdir Download\package\lib\net35-client
+if not exist Download\package\lib\net4 mkdir Download\package\lib\net4
+if not exist Download\package\lib\net4-client mkdir Download\package\lib\net4-client
 if not exist Download\package\lib\sl3-wp mkdir Download\package\lib\sl3-wp
 if not exist Download\package\lib\sl4 mkdir Download\package\lib\sl4
 
@@ -15,6 +17,8 @@ copy LICENSE.txt Download
 
 copy RestSharp\bin\Release\RestSharp.dll Download\Package\lib\net35\
 copy RestSharp\bin\Release\RestSharp.dll Download\Package\lib\net35-client\
+copy RestSharp.Net4\bin\Release\RestSharp.dll Download\Package\lib\net4\
+copy RestSharp.Net4\bin\Release\RestSharp.dll Download\Package\lib\net4-client\
 copy RestSharp.Silverlight\bin\Release\RestSharp.Silverlight.dll Download\Package\lib\sl4\
 copy RestSharp.WindowsPhone\bin\Release\RestSharp.WindowsPhone.dll Download\Package\lib\sl3-wp\
 
