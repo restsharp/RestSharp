8d5230b89897765c16669d17dec7a7158f039490
Andrew Young <andrewdyoung@gmail.com>
2011-08-13 11:14:14 -0700

    Added skipped oauth test for manual execution.

--- RestSharp.IntegrationTests/oAuth1Tests.cs
+++ RestSharp.IntegrationTests/oAuth1Tests.cs
@@ -11,7 +11,7 @@ namespace RestSharp.IntegrationTests
 {
 	public class oAuth1Tests
 	{
-		[Fact]
+		[Fact(Skip = "Provide your own consumer key/secret before running")]
 		public void Can_Authenticate_With_OAuth()
 		{
 			const string consumerKey = "";
