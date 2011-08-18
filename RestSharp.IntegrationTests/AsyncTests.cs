bf0f839b21ee70a94ed5130793cee3ad7f33cd9d
Andrew Young <andrewdyoung@gmail.com>
2011-08-17 20:44:30 -0700

    Removed console writes.

--- RestSharp.IntegrationTests/AsyncTests.cs
+++ RestSharp.IntegrationTests/AsyncTests.cs
@@ -16,16 +16,13 @@ namespace RestSharp.IntegrationTests
 			const string baseUrl = "http://localhost:8080/";
 			const string val = "Basic async test";
 			var resetEvent = new ManualResetEvent(false);
-			Console.WriteLine("before start server");
 			using (SimpleServer.Create(baseUrl, Handlers.EchoValue(val)))
 			{
 				var client = new RestClient(baseUrl);
 				var request = new RestRequest("");
 
-				Console.WriteLine("before call");
-				var asyncHandle = client.ExecuteAsync(request, response =>
+				client.ExecuteAsync(request, response =>
 				{
-					Console.WriteLine("response");
 					Assert.NotNull(response.Content);
 					Assert.Equal(val, response.Content);
 					resetEvent.Set();
