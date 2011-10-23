19d38547c4fcf878dddea42ea28ba7ffd85dbda5
johnsheehan <johnsheehan@gmail.com>
2011-10-22 23:43:20 -0700

    fixing refs again and bumping to 102.3

--- package.cmd
+++ package.cmd
@@ -1,17 +1,13 @@
-if not exist Download mkdir Download
-if not exist Download\WindowsPhone mkdir Download\WindowsPhone
 if not exist Download\WindowsPhone\7.0\ mkdir Download\WindowsPhone\7.0\
 if not exist Download\WindowsPhone\7.1\ mkdir Download\WindowsPhone\7.1\
-if not exist Download\package mkdir Download\package
-if not exist Download\package\lib mkdir Download\package\lib
 if not exist Download\package\lib\net35 mkdir Download\package\lib\net35
 if not exist Download\package\lib\net35-client mkdir Download\package\lib\net35-client
 if not exist Download\package\lib\sl3-wp mkdir Download\package\lib\sl3-wp
 if not exist Download\package\lib\sl4-wp71 mkdir Download\package\lib\sl4-wp71
 if not exist Download\package\lib\sl4 mkdir Download\package\lib\sl4
 
-tools\ilmerge.exe /lib:RestSharp\bin\Release /internalize /ndebug /v2 /out:Download\RestSharp.dll RestSharp.dll Newtonsoft.Json.Net35.dll
-tools\ilmerge.exe /lib:RestSharp.Silverlight\bin\Release /internalize /ndebug /targetplatform:v4,"C:\Program Files (x86)\Microsoft Silverlight\4.0.60129.0" /out:Download\RestSharp.Silverlight.dll RestSharp.Silverlight.dll Newtonsoft.Json.Silverlight.dll System.Xml.Linq.dll
+tools\ilmerge.exe /lib:RestSharp\bin\Release /internalize /ndebug /v2 /out:Download\RestSharp.dll RestSharp.dll Newtonsoft.Json.dll
+tools\ilmerge.exe /lib:RestSharp.Silverlight\bin\Release /internalize /ndebug /targetplatform:v4,"C:\Program Files (x86)\Microsoft Silverlight\4.0.60531.0" /out:Download\RestSharp.Silverlight.dll RestSharp.Silverlight.dll Newtonsoft.Json.dll System.Xml.Linq.dll
 
 copy RestSharp.WindowsPhone\bin\Release\*.dll Download\WindowsPhone\7.0\
 copy RestSharp.WindowsPhone.Mango\bin\Release\*.dll Download\WindowsPhone\7.1\
