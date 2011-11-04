ed93e79f2eb35704dd727a13d752bffb8a464eb6
Andrew Young <andrewdyoung@gmail.com>
2011-11-04 15:45:33 -0700

    Disabled range header for WP7

--- RestSharp/Http.cs
+++ RestSharp/Http.cs
@@ -178,7 +178,9 @@ namespace RestSharp
 			_restrictedHeaderActions.Add("Content-Type", (r, v) => r.ContentType = v);
 			_restrictedHeaderActions.Add("Date", (r, v) => { /* Set by system */ });
 			_restrictedHeaderActions.Add("Host", (r, v) => { /* Set by system */ });
+#if FRAMEWORK
 			_restrictedHeaderActions.Add("Range", (r, v) => { AddRange(r, v); });
+#endif
 		}
 
 		private const string FormBoundary = "-----------------------------28947758029299";
@@ -353,15 +355,19 @@ namespace RestSharp
 			}
 		}
 
-        private void AddRange(HttpWebRequest r, string range)
-        {
-            System.Text.RegularExpressions.Match m = System.Text.RegularExpressions.Regex.Match(range, "=(\\d+)-(\\d+)$");
-            if (m.Success)
-            {
-                int from = Convert.ToInt32(m.Groups[1].Value);
-                int to = Convert.ToInt32(m.Groups[2].Value);
-                r.AddRange(from, to);
-            }
+#if FRAMEWORK
+		private void AddRange(HttpWebRequest r, string range)
+		{
+			System.Text.RegularExpressions.Match m = System.Text.RegularExpressions.Regex.Match(range, "=(\\d+)-(\\d+)$");
+			if (!m.Success)
+			{
+				return;
+			}
+
+			int from = Convert.ToInt32(m.Groups[1].Value);
+			int to = Convert.ToInt32(m.Groups[2].Value);
+			r.AddRange(from, to);
 		}
+#endif
 	}
 }
