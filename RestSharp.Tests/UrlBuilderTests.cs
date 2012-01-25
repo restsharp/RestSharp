0f3aba6524bf99d73c36a981d59be7cec9e399d3
Michael Friis <friism@gmail.com>
2012-01-24 22:00:52 -0800

    add unit test to test empty request and bare hostname

--- RestSharp.Tests/UrlBuilderTests.cs
+++ RestSharp.Tests/UrlBuilderTests.cs
@@ -122,5 +122,17 @@ namespace RestSharp.Tests
 			Assert.Equal(expected, output);
 		}
 
+		[Fact]
+		public void GET_with_empty_request_and_bare_hostname()
+		{
+			var request = new RestRequest();
+			var client = new RestClient("http://example.com");
+
+			var expected = new Uri("http://example.com/");
+			var output = client.BuildUri(request);
+
+			Assert.Equal(expected, output);
+		}
+
 	}
 }
