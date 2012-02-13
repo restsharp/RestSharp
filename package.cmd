5766bf72c6c9f405f8b8f09ea1b11fd8561aa5fe
johnsheehan <johnsheehan@gmail.com>
2012-02-13 00:24:37 -0800

    including XML doc files in download

--- package.cmd
+++ package.cmd
@@ -8,11 +8,15 @@ if not exist Download\package\lib\sl4-wp71 mkdir Download\package\lib\sl4-wp71
 if not exist Download\package\lib\sl4 mkdir Download\package\lib\sl4
 
 tools\ilmerge.exe /lib:RestSharp\bin\Release /internalize /ndebug /v2 /out:Download\RestSharp.dll RestSharp.dll Newtonsoft.Json.dll
+copy RestSharp\bin\Release\RestSharp.xml Download\
 
 copy RestSharp.Silverlight\bin\Release\*.dll Download\Silverlight\
+copy RestSharp.Silverlight\bin\Release\*.xml Download\Silverlight\
 
 copy RestSharp.WindowsPhone\bin\Release\*.dll Download\WindowsPhone\7.0\
+copy RestSharp.WindowsPhone\bin\Release\*.xml Download\WindowsPhone\7.0\
 copy RestSharp.WindowsPhone.Mango\bin\Release\*.dll Download\WindowsPhone\7.1\
+copy RestSharp.WindowsPhone.Mango\bin\Release\*.xml Download\WindowsPhone\7.1\
 
 copy LICENSE.txt Download
 
@@ -21,5 +25,11 @@ copy RestSharp\bin\Release\RestSharp.dll Download\Package\lib\net35-client\
 copy RestSharp.Silverlight\bin\Release\RestSharp.Silverlight.dll Download\Package\lib\sl4\
 copy RestSharp.WindowsPhone\bin\Release\RestSharp.WindowsPhone.dll Download\Package\lib\sl3-wp\
 copy RestSharp.WindowsPhone.Mango\bin\Release\RestSharp.WindowsPhone.dll Download\Package\lib\sl4-wp71\
+
+copy RestSharp\bin\Release\RestSharp.xml Download\Package\lib\net35\
+copy RestSharp\bin\Release\RestSharp.xml Download\Package\lib\net35-client\
+copy RestSharp.Silverlight\bin\Release\RestSharp.Silverlight.xml Download\Package\lib\sl4\
+copy RestSharp.WindowsPhone\bin\Release\RestSharp.WindowsPhone.xml Download\Package\lib\sl3-wp\
+copy RestSharp.WindowsPhone.Mango\bin\Release\RestSharp.WindowsPhone.xml Download\Package\lib\sl4-wp71\
 
 tools\nuget.exe pack restsharp.nuspec -BasePath Download\Package -Output Download
