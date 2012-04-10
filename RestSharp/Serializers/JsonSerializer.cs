9d11af9d9604c51d072dc253422fdfeb5a22e199
John Sheehan <johnsheehan@gmail.com>
2012-04-09 18:40:50 -0700

    103 prerelease stuff
    swapped jsonserialier and misc packaging

--- RestSharp/Serializers/JsonSerializer.cs
+++ RestSharp/Serializers/JsonSerializer.cs
@@ -1,24 +1,4 @@
-﻿#region License
-//   Copyright 2010 John Sheehan
-//
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License. 
-#endregion
-#region Acknowledgements
-// Original JsonSerializer contributed by Daniel Crenna (@dimebrain)
-#endregion
-
-using System.IO;
-using Newtonsoft.Json;
+﻿using System.IO;
 
 namespace RestSharp.Serializers
 {
@@ -28,45 +8,22 @@ namespace RestSharp.Serializers
 	/// </summary>
 	public class JsonSerializer : ISerializer
 	{
-	    private readonly Newtonsoft.Json.JsonSerializer _serializer;
-        
-        /// <summary>
+		/// <summary>
 		/// Default serializer
 		/// </summary>
-		public JsonSerializer() {
+		public JsonSerializer()
+		{
 			ContentType = "application/json";
-            _serializer = new Newtonsoft.Json.JsonSerializer {
-                MissingMemberHandling = MissingMemberHandling.Ignore,
-                NullValueHandling = NullValueHandling.Include,
-                DefaultValueHandling = DefaultValueHandling.Include
-            };
 		}
-
-        /// <summary>
-        /// Default serializer with overload for allowing custom Json.NET settings
-        /// </summary>
-        public JsonSerializer(Newtonsoft.Json.JsonSerializer serializer){
-            ContentType = "application/json";
-            _serializer = serializer;
-        }
 
 		/// <summary>
 		/// Serialize the object as JSON
 		/// </summary>
 		/// <param name="obj">Object to serialize</param>
 		/// <returns>JSON as String</returns>
-		public string Serialize(object obj) {
-			using (var stringWriter = new StringWriter()) {
-				using (var jsonTextWriter = new JsonTextWriter(stringWriter)) {
-					jsonTextWriter.Formatting = Formatting.Indented;
-					jsonTextWriter.QuoteChar = '"';
-
-					_serializer.Serialize(jsonTextWriter, obj);
-
-					var result = stringWriter.ToString();
-					return result;
-				}
-			}
+		public string Serialize(object obj)
+		{
+			return SimpleJson.SerializeObject(obj);
 		}
 
 		/// <summary>
--- package.cmd
+++ package.cmd
@@ -1,4 +1,4 @@
-if not exist Download\Silverlight mkdir Download\Silverlight
+﻿if not exist Download\Silverlight mkdir Download\Silverlight
 if not exist Download\WindowsPhone\7.0\ mkdir Download\WindowsPhone\7.0\
 if not exist Download\WindowsPhone\7.1\ mkdir Download\WindowsPhone\7.1\
 if not exist Download\package\lib\net35 mkdir Download\package\lib\net35
@@ -19,6 +19,8 @@ copy RestSharp.WindowsPhone.Mango\bin\Release\*.dll Download\WindowsPhone\7.1\
 copy RestSharp.WindowsPhone.Mango\bin\Release\*.xml Download\WindowsPhone\7.1\
 
 copy LICENSE.txt Download
+copy readme.txt Download
+copy readme.txt Download\package
 
 copy RestSharp\bin\Release\RestSharp.dll Download\Package\lib\net35\
 copy RestSharp\bin\Release\RestSharp.dll Download\Package\lib\net35-client\
@@ -32,4 +34,5 @@ copy RestSharp.Silverlight\bin\Release\RestSharp.Silverlight.xml Download\Packag
 copy RestSharp.WindowsPhone\bin\Release\RestSharp.WindowsPhone.xml Download\Package\lib\sl3-wp\
 copy RestSharp.WindowsPhone.Mango\bin\Release\RestSharp.WindowsPhone.xml Download\Package\lib\sl4-wp71\
 
+tools\nuget.exe update -self
 tools\nuget.exe pack restsharp.nuspec -BasePath Download\Package -Output Download
