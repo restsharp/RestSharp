25b0fc187bdfefa578d88c224bc726d210cee405
jittuu <soemoetunlwin@gmail.com>
2012-05-08 18:14:18 +0800

    added CLSCompliant(true)

--- RestSharp/SharedAssemblyInfo.cs
+++ RestSharp/SharedAssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -12,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © RestSharp Project 2009-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
+[assembly: CLSCompliant(true)]
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
