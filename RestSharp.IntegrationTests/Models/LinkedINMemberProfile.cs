0afda7d3b005b645ca6a3716ec81895a8f99177e
Bert Willems <b.willems@premotion.nl>
2012-05-09 15:17:31 +0200

    added integration test for LinkedIN

---
+++ RestSharp.IntegrationTests/Models/LinkedINMemberProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace RestSharp.IntegrationTests.Models
+{
+	/// <summary>
+	/// Model for used by the LinkedIN integration tests. <see cref="oAuth1Tests.Can_Retrieve_Member_Profile_Field_Field_Selector_From_LinkedIN"/>.
+	/// </summary>
+	public class LinkedINMemberProfile
+	{
+		public string Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+}
