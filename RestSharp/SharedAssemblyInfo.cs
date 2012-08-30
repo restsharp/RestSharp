813ff3c453b092ab7040ceb3eb0d0f7a66f9c8fd
Andrew Young <andrewdyoung@gmail.com>
2012-08-30 00:35:45 -0700

    Version bump: 103.5

--- RestSharp/SharedAssemblyInfo.cs
+++ RestSharp/SharedAssemblyInfo.cs
@@ -24,5 +24,5 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("103.4.0.0")]
-[assembly: AssemblyFileVersion("103.4.0.0")]
+[assembly: AssemblyVersion("103.5.0.0")]
+[assembly: AssemblyFileVersion("103.5.0.0")]
