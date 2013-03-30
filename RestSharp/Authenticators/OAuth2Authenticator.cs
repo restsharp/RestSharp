80bad5ce7ef249fc457fb4f2c042711d38175790
Erik Schierboom <erik_schierboom@hotmail.com>
2013-03-30 10:43:59 +0100

    Added support for specifying token type in OAuth2Authenticator. fixes #364

--- RestSharp/Authenticators/OAuth2Authenticator.cs
+++ RestSharp/Authenticators/OAuth2Authenticator.cs
@@ -77,7 +77,7 @@ namespace RestSharp
 	public class OAuth2AuthorizationRequestHeaderAuthenticator : OAuth2Authenticator
 	{
 		/// <summary>
-		/// Stores the Authoriztion header value as "OAuth accessToken". used for performance.
+		/// Stores the Authorization header value as "[tokenType] accessToken". used for performance.
 		/// </summary>
 		private readonly string _authorizationValue;
 
@@ -88,10 +88,24 @@ namespace RestSharp
 		/// The access token.
 		/// </param>
 		public OAuth2AuthorizationRequestHeaderAuthenticator(string accessToken)
+			: this(accessToken, "OAuth")
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="OAuth2AuthorizationRequestHeaderAuthenticator"/> class.
+		/// </summary>
+		/// <param name="accessToken">
+		/// The access token.
+		/// </param>
+		/// <param name="tokenType">
+		/// The token type.
+		/// </param>
+		public OAuth2AuthorizationRequestHeaderAuthenticator(string accessToken, string tokenType)
 			: base(accessToken)
 		{
 			// Conatenate during constructor so that it is only done once. can improve performance.
-			_authorizationValue = "OAuth " + accessToken;
+			_authorizationValue = tokenType + " " + accessToken;
 		}
 
 		public override void Authenticate(IRestClient client, IRestRequest request)
