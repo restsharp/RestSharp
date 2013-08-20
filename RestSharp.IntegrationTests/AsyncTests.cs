dfd18ba0631322fcf46062fd77ba0481c0c1f0d0
Erik Schierboom <erik_schierboom@hotmail.com>
2013-08-20 09:32:06 +0200

    Added unit tests for task-based async methods

--- RestSharp.IntegrationTests/AsyncTests.cs
+++ RestSharp.IntegrationTests/AsyncTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading;
+using System.Net;
 using RestSharp.IntegrationTests.Helpers;
 using Xunit;
 
@@ -52,5 +50,146 @@ namespace RestSharp.IntegrationTests
 			}
 		}
 
+		[Fact]
+		public void Can_Perform_GET_TaskAsync()
+		{
+			const string baseUrl = "http://localhost:8080/";
+			const string val = "Basic async task test";
+			using (SimpleServer.Create(baseUrl, Handlers.EchoValue(val)))
+			{
+				var client = new RestClient(baseUrl);
+				var request = new RestRequest("");
+
+				var task = client.ExecuteTaskAsync(request);
+				task.Wait();
+
+				Assert.NotNull(task.Result.Content);
+				Assert.Equal(val, task.Result.Content);
+			}
+		}
+		
+		[Fact]
+		public void Can_Perform_GET_TaskAsync_With_Response_Type()
+		{
+			const string baseUrl = "http://localhost:8080/";
+			using (SimpleServer.Create(baseUrl, Handlers.Generic<ResponseHandler>()))
+			{
+				var client = new RestClient(baseUrl);
+				var request = new RestRequest("success");
+
+				var task = client.ExecuteTaskAsync<Response>(request);
+				task.Wait();
+
+				Assert.Equal("Works!", task.Result.Message);
+			}
+		}
+
+		[Fact]
+		public void Can_Cancel_GET_TaskAsync()
+		{
+			const string baseUrl = "http://localhost:8080/";
+			const string val = "Basic async task test";
+			using (SimpleServer.Create(baseUrl, Handlers.EchoValue(val)))
+			{
+				var client = new RestClient(baseUrl);
+				var request = new RestRequest("timeout");
+				var cancellationTokenSource = new CancellationTokenSource();
+
+				var task = client.ExecuteTaskAsync(request, cancellationTokenSource.Token);
+				cancellationTokenSource.Cancel();
+
+				Assert.True(task.IsCanceled);
+			}
+		}
+
+		[Fact]
+		public void Can_Cancel_GET_TaskAsync_With_Response_Type()
+		{
+			const string baseUrl = "http://localhost:8080/";
+			const string val = "Basic async task test";
+			using (SimpleServer.Create(baseUrl, Handlers.EchoValue(val)))
+			{
+				var client = new RestClient(baseUrl);
+				var request = new RestRequest("timeout");
+				var cancellationTokenSource = new CancellationTokenSource();
+
+				var task = client.ExecuteTaskAsync<Response>(request, cancellationTokenSource.Token);
+				cancellationTokenSource.Cancel();
+
+				Assert.True(task.IsCanceled);
+			}
+		}
+
+		[Fact]
+		public void Handles_GET_Request_Errors_TaskAsync()
+		{
+			const string baseUrl = "http://localhost:8080/";
+			using (SimpleServer.Create(baseUrl, UrlToStatusCodeHandler))
+			{
+				var client = new RestClient(baseUrl);
+				var request = new RestRequest("404");
+
+				var task = client.ExecuteTaskAsync(request);
+				task.Wait();
+
+				Assert.Equal(HttpStatusCode.NotFound, task.Result.StatusCode);
+			}
+		}
+
+		[Fact]
+		public void Handles_GET_Request_Errors_TaskAsync_With_Response_Type()
+		{
+			const string baseUrl = "http://localhost:8080/";
+			using (SimpleServer.Create(baseUrl, UrlToStatusCodeHandler))
+			{
+				var client = new RestClient(baseUrl);
+				var request = new RestRequest("404");
+
+				var task = client.ExecuteTaskAsync<Response>(request);
+				task.Wait();
+
+				Assert.Null(task.Result);
+			}
+		}
+
+		void UrlToStatusCodeHandler(HttpListenerContext obj)
+		{
+			obj.Response.StatusCode = int.Parse(obj.Request.Url.Segments.Last());
+		}
+
+		public class ResponseHandler
+		{
+			void error(HttpListenerContext context)
+			{
+				context.Response.StatusCode = 400;
+				context.Response.Headers.Add("Content-Type", "application/xml");
+				context.Response.OutputStream.WriteStringUtf8(
+	@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<Response>
+	<Error>
+		<Message>Not found!</Message>
+	</Error>
+</Response>");
+			}
+			void success(HttpListenerContext context)
+			{
+				context.Response.OutputStream.WriteStringUtf8(
+	@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<Response>
+	<Success>
+		<Message>Works!</Message>
+	</Success>
+</Response>");
+			}
+			void timeout(HttpListenerContext context)
+			{
+				Thread.Sleep(1000);
+			}
+		}
+
+		public class Response
+		{
+			public string Message { get; set; }
+		}
 	}
 }
