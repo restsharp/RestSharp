027a67719414c14778c94e00fa3175841ddd2b10
David Muto <david.muto@gmail.com>
2013-08-26 14:28:47 -0400

    moving version into a single spot

--- RestSharp/SharedAssemblyInfo.cs
+++ RestSharp/SharedAssemblyInfo.cs
@@ -24,5 +24,11 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("104.2.0.0")]
-[assembly: AssemblyFileVersion("104.2.0.0")]
+[assembly: AssemblyVersion(SharedAssemblyInfo.Version + ".0")]
+[assembly: AssemblyInformationalVersion(SharedAssemblyInfo.Version)]
+[assembly: AssemblyFileVersion(SharedAssemblyInfo.Version + ".0")]
+
+class SharedAssemblyInfo
+{
+    public const string Version = "104.2.0";
+}
