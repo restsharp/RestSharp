4e50d757b411d41748551b4f4f9e321957628461
Devin Rader <devin@twilio.com>
2013-09-09 19:25:48 -0400

    Renamed to bat and added unit tests

--- build.bat
+++ build.bat
@@ -10,8 +10,8 @@ REM %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Twilio.sln /p:Config
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
-REM "%GallioEcho%" RestSharp.Integration.Tests\bin\Release\RestSharp.IntegrationTests.dll
-REM if not "%errorlevel%"=="0" goto failure
+"%GallioEcho%" RestSharp.Integration.Tests\bin\Release\RestSharp.IntegrationTests.dll
+if not "%errorlevel%"=="0" goto failure
 
 
 rd Download /s /q  REM delete the old stuff
