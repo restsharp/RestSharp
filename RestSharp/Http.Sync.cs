7d4674da9485d4e6e6d8b9086aad180d22176155
Jesse Webb <gweebz@gmail.com>
2013-09-16 02:29:01 -0600

    RequestBody now supported by DELETE and OPTIONS

--- RestSharp/Http.Sync.cs
+++ RestSharp/Http.Sync.cs
@@ -111,6 +111,16 @@ namespace RestSharp
 		{
 			var webRequest = ConfigureWebRequest(method, Url);
 
+
+            if (method == "DELETE" || method == "OPTIONS")
+            {
+                if (HasBody)
+                {
+                    webRequest.ContentType = RequestContentType;
+                    WriteRequestBody(webRequest);
+                }
+            }
+
 			return GetResponse(webRequest);
 		}
 
