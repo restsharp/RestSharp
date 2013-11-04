4860f7f2b3d63ad8fae7c7b2c70af97c17585ba9
Jordy Langen <jordy.langen@mediaan.com>
2013-11-04 17:05:36 +0100

    Added support for deserializing arrays

---
+++ RestSharp.Tests/SampleClasses/FooWithArrayProperty.cs
@@ -0,0 +1,14 @@
+﻿namespace RestSharp.Tests.SampleClasses
+{
+    internal class FooWithArrayProperty
+    {
+        public string Name { get; set; }
+
+        public Id[] Ids { get; set; }
+    }
+
+    internal class Id
+    {
+        public int Value { get; set; }
+    }
+}
