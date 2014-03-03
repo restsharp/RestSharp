2c03bf9bbcb7471286beff6b193dbadd9916155f
Peter Hizalev <peter.hizalev@gmail.com>
2014-03-03 14:57:11 -0800

    Fix null reference on non-HTTP response exception

--- RestSharp/RestClient.Async.cs
+++ RestSharp/RestClient.Async.cs
@@ -153,12 +153,7 @@ namespace RestSharp
 
 		private void DeserializeResponse<T>(IRestRequest request, Action<IRestResponse<T>, RestRequestAsyncHandle> callback, IRestResponse response, RestRequestAsyncHandle asyncHandle)
 		{
-			IRestResponse<T> restResponse = response as RestResponse<T>;
-			if (response.ResponseStatus == ResponseStatus.Completed)
-			{
-				restResponse = Deserialize<T>(request, response);
-			}
-
+			IRestResponse<T> restResponse = Deserialize<T>(request, response);
 			callback(restResponse, asyncHandle);
 		}
 
