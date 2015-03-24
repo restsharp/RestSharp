fb9472b73b68c7c3f0da20329b88ae843a097062
Michael Hallett <michael.j.hallett@gmail.com>
2015-03-23 20:20:12 -0700

    added targeting for .net 4.5.1 and 4.5.2 framework editions

---
+++ RestSharp.Net45/Extensions/RestClientExtensions.cs
@@ -0,0 +1,17 @@
+﻿
+namespace RestSharp.Extensions
+{
+    public static partial class RestClientExtensions
+    {
+        public static RestResponse<dynamic> ExecuteDynamic(this IRestClient client, IRestRequest request)
+        {
+            var response = client.Execute<dynamic>(request);
+            var generic = (RestResponse<dynamic>)response;
+            dynamic content = SimpleJson.DeserializeObject(response.Content);
+
+            generic.Data = content;
+
+            return generic;
+        }
+    }
+}
--- build.bat
+++ build.bat
@@ -5,14 +5,12 @@ if "%config%" == "" (
 )
 
 REM Build
-REM %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Twilio.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:true /p:BuildInParallel=true /p:RestorePackages=true /t:Rebuild
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe RestSharp.sln /p:Configuration=%config% /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:true /p:BuildInParallel=true /p:RestorePackages=true /t:Clean,Rebuild
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
 REM "%GallioEcho%" RestSharp.IntegrationTests\bin\Release\RestSharp.IntegrationTests.dll
 REM if not "%errorlevel%"=="0" goto failure
-
 
 rd Download /s /q  REM delete the old stuff
 
@@ -23,7 +21,13 @@ if not exist Download\package\lib\net35 mkdir Download\package\lib\net35\
 if not exist Download\package\lib\net35-client mkdir Download\package\lib\net35-client\
 if not exist Download\package\lib\net4 mkdir Download\package\lib\net4\
 if not exist Download\package\lib\net4-client mkdir Download\package\lib\net4-client\
-if not exist Download\package\lib\sl4-wp71 mkdir Download\package\lib\sl4-wp71\
+if not exist Download\package\lib\net45 mkdir Download\package\lib\net45\
+if not exist Download\package\lib\net45-client mkdir Download\package\lib\net45-client\
+if not exist Download\package\lib\net451 mkdir Download\package\lib\net451\
+if not exist Download\package\lib\net451-client mkdir Download\package\lib\net451-client\
+if not exist Download\package\lib\net452 mkdir Download\package\lib\net452\
+if not exist Download\package\lib\net452-client mkdir Download\package\lib\net452-client\
+if not exist Download\package\lib\windowsphone8 mkdir Download\package\lib\windowsphone8\
 if not exist Download\package\lib\sl4 mkdir Download\package\lib\sl4\
 
 copy LICENSE.txt Download\
@@ -36,7 +40,7 @@ copy RestSharp.Net4\bin\Release\RestSharp.dll Download\Package\lib\net4\
 copy RestSharp.Net4\bin\Release\RestSharp.dll Download\Package\lib\net4-client\
 
 copy RestSharp.Silverlight\bin\Release\RestSharp.Silverlight.dll Download\Package\lib\sl4\
-copy RestSharp.WindowsPhone\bin\Release\RestSharp.WindowsPhone.dll Download\Package\lib\sl4-wp71\
+copy RestSharp.WindowsPhone\bin\Release\RestSharp.WindowsPhone.dll Download\Package\lib\windowsphone8\
 
 copy RestSharp\bin\Release\RestSharp.xml Download\Package\lib\net35\
 copy RestSharp\bin\Release\RestSharp.xml Download\Package\lib\net35-client\
@@ -44,8 +48,17 @@ copy RestSharp\bin\Release\RestSharp.xml Download\Package\lib\net35-client\
 copy RestSharp.Net4\bin\Release\RestSharp.xml Download\Package\lib\net4\
 copy RestSharp.Net4\bin\Release\RestSharp.xml Download\Package\lib\net4-client\
 
+copy RestSharp.Net45\bin\Release\RestSharp.xml Download\Package\lib\net45\
+copy RestSharp.Net45\bin\Release\RestSharp.xml Download\Package\lib\net45-client\
+
+copy RestSharp.Net451\bin\Release\RestSharp.xml Download\Package\lib\net451\
+copy RestSharp.Net451\bin\Release\RestSharp.xml Download\Package\lib\net451-client\
+
+copy RestSharp.Net452\bin\Release\RestSharp.xml Download\Package\lib\net452\
+copy RestSharp.Net452\bin\Release\RestSharp.xml Download\Package\lib\net452-client\
+
 copy RestSharp.Silverlight\bin\Release\RestSharp.Silverlight.xml Download\Package\lib\sl4\
-copy RestSharp.WindowsPhone\bin\Release\RestSharp.WindowsPhone.xml Download\Package\lib\sl4-wp71\
+copy RestSharp.WindowsPhone\bin\Release\RestSharp.WindowsPhone.xml Download\Package\lib\windowsphone8\
 
 %nuget% pack "restsharp-computed.nuspec" -BasePath Download\Package -Output Download
 if not "%errorlevel%"=="0" goto failure
