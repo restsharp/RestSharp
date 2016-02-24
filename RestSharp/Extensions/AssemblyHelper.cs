9376b7fb562b06694dbacc2f47924b9abe2bb316
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-02-24 20:02:53 +0100

    Add AssemblyHelper.cs

---
+++ RestSharp/Extensions/AssemblyHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace RestSharp.Extensions
+{
+    public static class AssemblyHelper
+    {
+        public static Version GetVersion(Type type)
+        {
+#if WINDOWS_UWP || DNXCORE50
+        var asm = type.GetTypeInfo().Assembly;
+        return new AssemblyName(asm.FullName).Version;    
+#else
+            return new AssemblyName(Assembly.GetExecutingAssembly().FullName).Version;
+#endif
+        }
+    }
+}
