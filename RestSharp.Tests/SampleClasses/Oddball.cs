41fb74f1e7f0c3f51f61da34fdcb7c2a85279779
Egor Ilchenko <zzodoo@gmail.com>
2016-09-25 18:37:53 +0700

    Added unit test

--- RestSharp.Tests/SampleClasses/Oddball.cs
+++ RestSharp.Tests/SampleClasses/Oddball.cs
@@ -9,5 +9,8 @@
 
         [Deserializers.DeserializeAs(Name = "oddballPropertyName")]
         public string GoodPropertyName { get; set; }
+
+        [Deserializers.DeserializeAs(Name = "oddballListName")]
+        public System.Collections.Generic.List<string> ListWithGoodName { get; set; } 
     }
 }
