99d1324826212f105a47eed0204dd3f75e923bde
Robert Hernandez <rhernandez3@lenovo.com>
2017-03-09 10:26:12 -0500

    adding support for Int64 ranges

--- RestSharp.Tests/AddRangeTests.cs
+++ RestSharp.Tests/AddRangeTests.cs
@@ -14,5 +14,17 @@ namespace RestSharp.Tests
             req.AddHeader("Range", "pages=1-2");
             restClient.Execute(req);
         }
+
+        [Test]
+        public void ShouldParseOutLongRangeSpecifier()
+        {
+            RestClient restClient = new RestClient("http://localhost");
+            RestRequest req = new RestRequest("bob", Method.GET);
+            long start = (long)int.MaxValue + 1;
+            long end = start + 1;
+
+            req.AddHeader("Range", string.Format("pages={0}-{1}", start, end));
+            restClient.Execute(req);
+        }
     }
 }
