6f1e4a04c9c20086f8d48c49b9bb4e45e7c1c3ee
Alexey Zimarev <alex@zimarev.com>
2018-10-11 20:39:51 +0200

    Revert "Fixed double escape bug in SimpleJson"

--- RestSharp/SimpleJson.cs
+++ RestSharp/SimpleJson.cs
@@ -606,7 +606,6 @@ namespace SimpleJson
             StringBuilder builder = new StringBuilder(BUILDER_CAPACITY);
             bool success = SerializeValue(jsonSerializerStrategy, json, builder);
             return (success ? builder.ToString() : null);
-            
         }
 
         public static string SerializeObject(object json)
@@ -1010,21 +1009,7 @@ namespace SimpleJson
             bool success = true;
             string stringValue = value as string;
             if (stringValue != null)
-            {
-                // check for json formatting if we're already a string
-                string trimmed = stringValue.Trim();
-                if (trimmed.StartsWith("{") && trimmed.EndsWith("}"))
-                {
-                    object tmp;
-                    success = TryDeserializeObject(trimmed, out tmp);
-                    if (success)
-                        builder.Append(trimmed);
-                }
-                else
-                {
-                    success = SerializeString(stringValue, builder);
-                }
-            }
+                success = SerializeString(stringValue, builder);
             else
             {
                 IDictionary<string, object> dict = value as IDictionary<string, object>;
