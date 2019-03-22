7d3a7d841655460a419c876090fb496cf9c7d0dc
jnyrup <jnyrup@gmail.com>
2019-03-22 15:47:01 +0100

    Add APPVEYOR constant instead of replacing existing constants

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 dotnet restore
-msbuild /t:build /p:Configuration=Release /p:DefineConstants=APPVEYOR
+msbuild /t:build /p:Configuration=Release /p:AppVeyor=true
 dotnet test --no-build
 msbuild /t:Pack
