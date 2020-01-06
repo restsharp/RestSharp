7d71ac52bbc1cc697d7fa54e3b9630723f499f07
Alexey Zimarev <alex@zimarev.com>
2020-01-06 23:21:43 +0100

    Moved files, added JsonNet serializer

--- src/RestSharp/Serialization/Json/JsonSerializer.cs
+++ src/RestSharp/Serialization/Json/JsonSerializer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Xml;
 using RestSharp.Deserializers;
 using RestSharp.Extensions;
--- src/RestSharp/Serialization/Xml/XmlRestSerializer.cs
+++ src/RestSharp/Serialization/Xml/XmlRestSerializer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Globalization;
+using System.Text;
 using RestSharp.Deserializers;
 using RestSharp.Serializers;
 
