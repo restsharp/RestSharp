3512af427266e4e1d922d3b987db1b2e234ae478
Amos Onn <aonn@its.jnj.com>
2022-02-07 14:56:41 +0100

    Test strange-cased content headers

---
+++ test/RestSharp.Tests/RestContentTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace RestSharp.Tests;
+
+public class RestContentTests {
+    [Fact]
+    public void RestContent_CaseInsensitiveHeaders() {
+        var myContentType = "application/x-custom";
+        var request = new RestRequest("resource");
+        request.AddHeader("coNteNt-TypE", myContentType);
+        var client = new RestClient();
+        var content = new RequestContent(client, request);
+
+        var httpContent = content.BuildContent();
+
+        Assert.Equal(myContentType, httpContent.Headers.ContentType.MediaType);
+    }
+}
