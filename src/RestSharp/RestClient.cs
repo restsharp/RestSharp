17a3532479809eb0439249c7a942ae4f6b41f3d2
Alexey Zimarev <alex@zimarev.com>
2022-03-13 20:01:17 +0100

    Proposed fix for #1765 (#1782)
    * Fix for #1765

--- src/RestSharp/RestClient.cs
+++ src/RestSharp/RestClient.cs
@@ -45,7 +45,7 @@ public partial class RestClient : IDisposable {
 
     HttpClient HttpClient { get; }
 
-    internal RestClientOptions Options { get; }
+    public RestClientOptions Options { get; }
 
     public RestClient(RestClientOptions options, Action<HttpRequestHeaders>? configureDefaultHeaders = null) {
         UseDefaultSerializers();
@@ -83,19 +83,32 @@ public partial class RestClient : IDisposable {
     /// <param name="baseUrl"></param>
     public RestClient(string baseUrl) : this(new Uri(Ensure.NotEmptyString(baseUrl, nameof(baseUrl)))) { }
 
-    public RestClient(HttpClient httpClient, RestClientOptions? options = null, bool disposeHttpClient = false) {
-        if (options?.CookieContainer != null) {
+    public RestClient(HttpClient httpClient, bool disposeHttpClient = false) {
+        UseDefaultSerializers();
+
+        HttpClient         = httpClient;
+        CookieContainer    = new CookieContainer();
+        Options            = new RestClientOptions();
+        _disposeHttpClient = disposeHttpClient;
+
+        if (httpClient.BaseAddress != null) {
+            Options.BaseUrl = httpClient.BaseAddress;
+        }
+    }
+
+    public RestClient(HttpClient httpClient, RestClientOptions options, bool disposeHttpClient = false) {
+        if (options.CookieContainer != null) {
             throw new ArgumentException("Custom cookie container cannot be added to the HttpClient instance", nameof(options.CookieContainer));
         }
 
         UseDefaultSerializers();
 
         HttpClient         = httpClient;
-        Options            = options ?? new RestClientOptions();
         CookieContainer    = new CookieContainer();
+        Options            = options;
         _disposeHttpClient = disposeHttpClient;
 
-        if (httpClient.BaseAddress != null && Options.BaseUrl == null) {
+        if (httpClient.BaseAddress != null && options.BaseUrl == null) {
             Options.BaseUrl = httpClient.BaseAddress;
         }
 
@@ -108,7 +121,7 @@ public partial class RestClient : IDisposable {
     /// </summary>
     /// <param name="handler">Message handler instance to use for HttpClient</param>
     /// <param name="disposeHandler">Dispose the handler when disposing RestClient, true by default</param>
-    public RestClient(HttpMessageHandler handler, bool disposeHandler = true) : this(new HttpClient(handler, disposeHandler), null, true) { }
+    public RestClient(HttpMessageHandler handler, bool disposeHandler = true) : this(new HttpClient(handler, disposeHandler), true) { }
 
     void ConfigureHttpClient(HttpClient httpClient) {
         if (Options.Timeout > 0) httpClient.Timeout = TimeSpan.FromMilliseconds(Options.Timeout);
@@ -159,7 +172,7 @@ public partial class RestClient : IDisposable {
             );
 
         if (!Options.AllowMultipleDefaultParametersWithSameName &&
-            !MultiParameterTypes.Contains(parameter.Type)       &&
+            !MultiParameterTypes.Contains(parameter.Type) &&
             DefaultParameters.Any(x => x.Name == parameter.Name)) {
             throw new ArgumentException("A default parameters with the same name has already been added", nameof(parameter));
         }
