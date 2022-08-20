327593107bf00e8d4864d18c74fda30eed072200
ishanpranav <ishan.pranav@outlook.com>
2022-08-19 18:03:46 -0700

    Add RestSharp.Serializers.CsvHelper

---
+++ src/RestSharp.Serializers.CsvHelper/CsvHelperSerializer.cs
@@ -0,0 +1,139 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections;
+using System.Globalization;
+using System.Reflection;
+
+namespace RestSharp.Serializers.CsvHelper {
+    public class CsvHelperSerializer : IDeserializer, IRestSerializer, ISerializer {
+        private const string TextCsvContentType = "text/csv";
+
+        private readonly CsvConfiguration _configuration;
+
+        public ISerializer Serializer {
+            get {
+                return this;
+            }
+        }
+
+        public IDeserializer Deserializer {
+            get {
+                return this;
+            }
+        }
+
+        public string[] AcceptedContentTypes {
+            get {
+                return new string[] { TextCsvContentType, "application/x-download" };
+            }
+        }
+
+        public SupportsContentType SupportsContentType {
+            get {
+                return x => Array.IndexOf(AcceptedContentTypes, x) != -1 || x.Contains("csv");
+            }
+        }
+
+        public DataFormat DataFormat {
+            get {
+                return DataFormat.None;
+            }
+        }
+
+        public string ContentType { get; set; } = TextCsvContentType;
+
+        public CsvHelperSerializer() {
+            _configuration = new CsvConfiguration(CultureInfo.InvariantCulture);
+        }
+
+        public CsvHelperSerializer(CsvConfiguration configuration) {
+            _configuration = configuration;
+        }
+
+        public T? Deserialize<T>(RestResponse response) {
+            try {
+                if (response.Content == null) {
+                    throw new InvalidOperationException(message: "Response content is null");
+                }
+                else {
+                    using (StringReader stringReader = new StringReader(response.Content))
+                    using (CsvReader csvReader = new CsvReader(stringReader, CultureInfo.CurrentCulture)) {
+                        Type? @interface = typeof(T).GetInterface("IEnumerable`1");
+
+                        if (@interface == null) {
+                            csvReader.Read();
+
+                            return csvReader.GetRecord<T>();
+                        }
+                        else {
+                            Type itemType = @interface.GenericTypeArguments[0];
+                            T result;
+
+                            try {
+                                result = Activator.CreateInstance<T>();
+                            }
+                            catch (MissingMethodException) {
+                                throw new InvalidOperationException(message: "The type must contain a public, parameterless constructor.");
+                            }
+
+                            MethodInfo? method = typeof(T).GetMethod(name: "Add");
+
+                            if (method == null) {
+                                throw new InvalidOperationException(message: "If the type implements IEnumerable<T>, then it must contain a public \"Add(T)\" method.");
+                            }
+                            else {
+                                foreach (object record in csvReader.GetRecords(itemType)) {
+                                    method.Invoke(result, new object[]
+                                    {
+                                        record
+                                    });
+                                }
+                            }
+
+                            return result;
+                        }
+                    }
+                }
+            }
+            catch (Exception exception) {
+                throw new DeserializationException(response, exception);
+            }
+        }
+
+        public string? Serialize(Parameter parameter) {
+            return Serialize(parameter.Value);
+        }
+
+        public string? Serialize(object? obj) {
+            if (obj == null) {
+                return null;
+            }
+            else {
+                using (StringWriter stringWriter = new StringWriter())
+                using (CsvWriter csvWriter = new CsvWriter(stringWriter, _configuration)) {
+                    if (obj is IEnumerable records) {
+                        IEnumerator enumerator = records.GetEnumerator();
+
+                        if (enumerator.MoveNext()) {
+                            csvWriter.WriteHeader(enumerator.Current.GetType());
+                            csvWriter.NextRecord();
+                            csvWriter.WriteRecords(records);
+                        }
+
+                        if (enumerator is IDisposable disposable) {
+                            disposable.Dispose();
+                        }
+                    }
+                    else {
+                        csvWriter.WriteHeader(obj.GetType());
+                        csvWriter.NextRecord();
+                        csvWriter.WriteRecord(obj);
+                        csvWriter.NextRecord();
+                    }
+
+                    return stringWriter.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ test/RestSharp.Tests.Serializers.Csv/CsvHelperTests.cs
@@ -0,0 +1,156 @@
+using CsvHelper.Configuration;
+using RestSharp.Serializers.CsvHelper;
+using RestSharp.Serializers.Json;
+using RestSharp.Tests.Shared.Extensions;
+using RestSharp.Tests.Shared.Fixtures;
+using System.Globalization;
+using System.Net;
+using System.Text;
+
+namespace RestSharp.Tests.Serializers.Csv {
+    public class CsvHelperTests {
+        private static readonly Fixture Fixture = new Fixture();
+
+        [Fact]
+        public async Task Use_CsvHelper_For_Response() {
+            var expected = Fixture.Create<TestObject>();
+
+            expected.DateTimeValue = new DateTime(expected.DateTimeValue.Year, expected.DateTimeValue.Month, expected.DateTimeValue.Day, expected.DateTimeValue.Hour, expected.DateTimeValue.Minute, expected.DateTimeValue.Second);
+
+            using var server = HttpServerFixture.StartServer(
+                (_, response) => {
+                    var serializer = new CsvHelperSerializer();
+
+                    response.ContentType = "text/csv";
+                    response.ContentEncoding = Encoding.UTF8;
+                    response.OutputStream.WriteStringUtf8(serializer.Serialize(expected)!);
+                }
+            );
+
+            var client = new RestClient(server.Url).UseCsvHelper();
+
+            var actual = await client.GetAsync<TestObject>(new RestRequest());
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public async Task Use_CsvHelper_For_Collection_Response() {
+            var count = Fixture.Create<int>();
+            var expected = new List<TestObject>(count);
+
+            for (int i = 0; i < count; i++) {
+                var item = Fixture.Create<TestObject>();
+
+                item.DateTimeValue = new DateTime(item.DateTimeValue.Year, item.DateTimeValue.Month, item.DateTimeValue.Day, item.DateTimeValue.Hour, item.DateTimeValue.Minute, item.DateTimeValue.Second);
+
+                expected.Add(item);
+            }
+
+            using var server = HttpServerFixture.StartServer(
+                (_, response) => {
+                    var serializer = new CsvHelperSerializer();
+
+                    response.ContentType = "text/csv";
+                    response.ContentEncoding = Encoding.UTF8;
+                    response.OutputStream.WriteStringUtf8(serializer.Serialize(expected));
+                }
+            );
+
+            var client = new RestClient(server.Url).UseCsvHelper();
+
+            var actual = await client.GetAsync<List<TestObject>>(new RestRequest());
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public async Task DeserilizationFails_IsSuccessfull_Should_BeFalse() {
+            using var server = HttpServerFixture.StartServer(
+                (_, response) => {
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.ContentType = "text/csv";
+                    response.ContentEncoding = Encoding.UTF8;
+                    response.OutputStream.WriteStringUtf8("invalid csv");
+                }
+            );
+
+            var client = new RestClient(server.Url).UseCsvHelper();
+
+            var response = await client.ExecuteAsync<TestObject>(new RestRequest());
+
+            response.IsSuccessStatusCode.Should().BeTrue();
+            response.IsSuccessful.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task DeserilizationSucceeds_IsSuccessfull_Should_BeTrue() {
+            var item = Fixture.Create<TestObject>();
+
+            using var server = HttpServerFixture.StartServer(
+                (_, response) => {
+                    var serializer = new SystemTextJsonSerializer();
+
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.ContentType = "text/csv";
+                    response.ContentEncoding = Encoding.UTF8;
+                    response.OutputStream.WriteStringUtf8(serializer.Serialize(item)!);
+                }
+            );
+
+            var client = new RestClient(server.Url).UseSystemTextJson();
+
+            var response = await client.ExecuteAsync<TestObject>(new RestRequest());
+
+            response.IsSuccessStatusCode.Should().BeTrue();
+            response.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task SerializedObject_Should_Be() {
+            var serializer = new CsvHelperSerializer(new CsvConfiguration(CultureInfo.InvariantCulture) {
+                NewLine = ";"
+            });
+
+            var item = new TestObject() {
+                Int32Value = 32,
+                SingleValue = 16.5f,
+                StringValue = "hello",
+                TimeSpanValue = TimeSpan.FromMinutes(10),
+                DateTimeValue = new DateTime(2024, 1, 20)
+            };
+
+            serializer.Serialize(item).Should().Be("StringValue,Int32Value,DecimalValue,DoubleValue,SingleValue,DateTimeValue,TimeSpanValue;hello,32,0,0,16.5,01/20/2024 00:00:00,00:10:00;");
+        }
+
+        [Fact]
+        public async Task SerializedCollection_Should_Be() {
+            var serializer = new CsvHelperSerializer(new CsvConfiguration(CultureInfo.InvariantCulture) {
+                NewLine = ";"
+            });
+
+            var items = new TestObject[] {
+                new TestObject() {
+                    Int32Value = 32,
+                    SingleValue = 16.5f,
+                    StringValue = "hello",
+                    TimeSpanValue = TimeSpan.FromMinutes(10),
+                    DateTimeValue = new DateTime(2024, 1, 20)
+                },
+                new TestObject() {
+                    Int32Value = 65,
+                    DecimalValue = 89.555m,
+                    TimeSpanValue = TimeSpan.FromSeconds(61),
+                    DateTimeValue = new DateTime(2022, 8, 19, 5, 15, 21)
+                },
+                new TestObject() {
+                    SingleValue = 80000,
+                    DoubleValue = 20.00001,
+                    StringValue = "String, with comma"
+                }
+            };
+
+            serializer.Serialize(items).Should().Be("StringValue,Int32Value,DecimalValue,DoubleValue,SingleValue,DateTimeValue,TimeSpanValue;hello,32,0,0,16.5,01/20/2024 00:00:00,00:10:00;,65,89.555,0,0,08/19/2022 05:15:21,00:01:01;\"String, with comma\",0,0,20.00001,80000,01/01/0001 00:00:00,00:00:00;");
+        }
+    }
+}
