1c62c3de08ef4e218ca66b0429134f6a12cc5ab6
Alexey Zimarev <alex@zimarev.com>
2022-09-20 09:26:42 +0200

    A bit more code style cleanup

--- test/RestSharp.Tests.Serializers.Csv/TestObject.cs
+++ test/RestSharp.Tests.Serializers.Csv/TestObject.cs
@@ -1,11 +1,11 @@
-﻿namespace RestSharp.Tests.Serializers.Csv {
-    internal class TestObject {
-        public string StringValue { get; set; }
-        public int Int32Value { get; set; }
-        public decimal DecimalValue { get; set; }
-        public double DoubleValue { get; set; }
-        public float SingleValue { get; set; }
-        public DateTime DateTimeValue { get; set; }
-        public TimeSpan TimeSpanValue { get; set; }
-    }
+﻿namespace RestSharp.Tests.Serializers.Csv; 
+
+class TestObject {
+    public string   StringValue   { get; set; }
+    public int      Int32Value    { get; set; }
+    public decimal  DecimalValue  { get; set; }
+    public double   DoubleValue   { get; set; }
+    public float    SingleValue   { get; set; }
+    public DateTime DateTimeValue { get; set; }
+    public TimeSpan TimeSpanValue { get; set; }
 }
