79848da8027ff8ea826eecf86ea8a83cfb131c76
Alexey Zimarev <alex@zimarev.com>
2022-09-22 12:21:47 +0200

    .NET Foundation update (#1934)

--- src/RestSharp/Serializers/SerializerRecord.cs
+++ src/RestSharp/Serializers/SerializerRecord.cs
@@ -1,4 +1,4 @@
-//  Copyright © 2009-2021 John Sheehan, Andrew Young, Alexey Zimarev and RestSharp community
+//  Copyright (c) .NET Foundation and Contributors
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
