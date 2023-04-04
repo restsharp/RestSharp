b8d7ec87e2720f48dfe3f05811737564f74e1a17
Franz Reinhard Seidl <franz.seidl@bauradar.at>
2023-04-04 15:13:10 +0200

    feature: Added Interfaces for Interceptors

---
+++ src/RestSharp/Interceptors/IInterceptor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestSharp.Interceptors {
+   
+    /// <summary>
+    /// Interceptor, which intercepts outgoing requests and incoming responses
+    /// </summary>
+    public interface IInterceptor: IBeforeSerializationInterceptor,IBeforeRequestInterceptor, IAfterRequestInterceptor, IBeforeDeserializeInterceptor {
+
+        
+    }
+}
