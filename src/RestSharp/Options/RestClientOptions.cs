d8e1e9a93bf7f226660280a9a46ab817d667ca20
Alexey Zimarev <alex@zimarev.com>
2023-04-04 16:14:04 +0200

    Annotate platform-specific properties and check the platform when configuring the handler (#2044)

--- src/RestSharp/Options/RestClientOptions.cs
+++ src/RestSharp/Options/RestClientOptions.cs
@@ -17,10 +17,15 @@ using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Security;
 using System.Reflection;
+using System.Runtime.Versioning;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using RestSharp.Authenticators;
 using RestSharp.Extensions;
+
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable PropertyCanBeMadeInitOnly.Global
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
 
 namespace RestSharp;
 
@@ -62,6 +67,9 @@ public class RestClientOptions {
     /// <summary>
     /// Passed to <see cref="HttpMessageHandler"/> <code>Credentials</code> property
     /// </summary>
+#if NET
+    [UnsupportedOSPlatform("browser")]
+#endif
     public ICredentials? Credentials { get; set; }
 
     /// <summary>
@@ -69,6 +77,9 @@ public class RestClientOptions {
     /// running) will be sent along to the server. The default is false.
     /// Passed to <see cref="HttpMessageHandler"/> <code>UseDefaultCredentials</code> property
     /// </summary>
+#if NET
+    [UnsupportedOSPlatform("browser")]
+#endif
     public bool UseDefaultCredentials { get; set; }
 
     /// <summary>
@@ -80,6 +91,7 @@ public class RestClientOptions {
     /// Set the decompression method to use when making requests
     /// </summary>
 #if NET
+    [UnsupportedOSPlatform("browser")]
     public DecompressionMethods AutomaticDecompression { get; set; } = DecompressionMethods.All;
 #else
     public DecompressionMethods AutomaticDecompression { get; set; } = DecompressionMethods.GZip;
@@ -88,16 +100,27 @@ public class RestClientOptions {
     /// <summary>
     /// Set the maximum number of redirects to follow
     /// </summary>
+#if NET
+    [UnsupportedOSPlatform("browser")]
+#endif
     public int? MaxRedirects { get; set; }
 
     /// <summary>
     /// X509CertificateCollection to be sent with request
     /// </summary>
+#if NET
+    [UnsupportedOSPlatform("browser")]
+#endif
     public X509CertificateCollection? ClientCertificates { get; set; }
 
     /// <summary>
     /// Set the proxy to use when making requests. Default is null, which will use the default system proxy if one is set.
     /// </summary>
+#if NET
+    [UnsupportedOSPlatform("browser")]
+    [UnsupportedOSPlatform("ios")]
+    [UnsupportedOSPlatform("tvos")]
+#endif
     public IWebProxy? Proxy { get; set; }
 
     /// <summary>
@@ -123,12 +146,18 @@ public class RestClientOptions {
     /// <summary>
     /// Passed to <see cref="HttpMessageHandler"/> <see langword="PreAuthenticate"/> property
     /// </summary>
+#if NET
+    [UnsupportedOSPlatform("browser")]
+#endif
     public bool PreAuthenticate { get; set; }
 
     /// <summary>
     /// Callback function for handling the validation of remote certificates. Useful for certificate pinning and
     /// overriding certificate errors in the scope of a request.
     /// </summary>
+#if NET
+    [UnsupportedOSPlatform("browser")]
+#endif
     public RemoteCertificateValidationCallback? RemoteCertificateValidationCallback { get; set; }
 
     /// <summary>
