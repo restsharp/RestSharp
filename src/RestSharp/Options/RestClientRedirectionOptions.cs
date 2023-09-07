487f138905165a78a78b87530e092c6a877f80e7
tuttb <bill.tutt@hill-rom.com>
2023-09-07 01:00:17 -0400

    Add new files..

---
+++ src/RestSharp/Options/RestClientRedirectionOptions.cs
@@ -0,0 +1,36 @@
+﻿using RestSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection;
+using System.Text;
+
+namespace RestSharp.Options {
+    [GenerateImmutable]
+    public class RestClientRedirectionOptions {
+        static readonly Version Version = new AssemblyName(typeof(RestClientOptions).Assembly.FullName!).Version!;
+
+        public bool FollowRedirects { get; set; } = true;
+        public bool FollowRedirectsToInsecure { get; set; } = false;
+        public bool ForwardHeaders { get; set; } = true;
+        public bool ForwardAuthorization { get; set; } = false;
+        public bool ForwardCookies { get; set; } = true;
+        public bool ForwardBody { get; set; } = true;
+        public bool ForwardQuery { get; set; } = true;
+        public int MaxRedirects { get; set; }
+        public bool ForwardFragment { get; set; } = true;
+        public IReadOnlyList<HttpStatusCode> RedirectStatusCodes { get; set; }
+
+        public RestClientRedirectionOptions() {
+            RedirectStatusCodes = new List<HttpStatusCode>() {
+                HttpStatusCode.MovedPermanently,
+                HttpStatusCode.SeeOther,
+                HttpStatusCode.TemporaryRedirect,
+                HttpStatusCode.Redirect,
+    #if NET
+                HttpStatusCode.PermanentRedirect,
+    #endif
+            }.AsReadOnly();
+        }
+    }
+}
---
+++ test/RestSharp.Tests.Integrated/Server/Handlers/RedirectWithStatusCodeResult.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharp.Tests.Integrated.Server.Handlers;
+
+/// <summary>
+/// An <see cref="IResult"/> that returns a redirection with a supplied status code value. 
+/// Created in order to easily return a SeeOther status code.
+/// </summary>
+class RedirectWithStatusCodeResult : IResult {
+    public int StatusCode { get; }
+    public string Uri { get; }
+
+    public RedirectWithStatusCodeResult(int statusCode, string url) {
+        Uri = url;
+        StatusCode = statusCode;
+    }
+
+    public Task ExecuteAsync(HttpContext httpContext) {
+        ArgumentNullException.ThrowIfNull(httpContext);
+
+        httpContext.Response.StatusCode = StatusCode;
+        httpContext.Response.Headers.Location = Uri;
+
+        return Task.CompletedTask;
+    }
+}
