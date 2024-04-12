e32c9dd9addbe4bee39279892efa99b36e66d3c7
Alexey Zimarev <alex@zimarev.com>
2024-04-12 20:29:57 +0200

    Doc files

---
+++ docs/README.md
@@ -0,0 +1,26 @@
+# Website
+
+This is a RestSharp documentation website built using [Docusaurus](https://docusaurus.io/), a modern static website generator.
+
+### Installation
+
+```
+$ yarn
+```
+
+### Local Development
+
+```
+$ yarn start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+### Build
+
+```
+$ yarn build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,102 @@
+import type {Config} from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+import {themes} from "prism-react-renderer";
+
+const config: Config = {
+  title: "RestSharp",
+  tagline: "Simple REST and HTTP API Client for .NET",
+  favicon: "img/favicon.ico",
+  url: "https://restsharp.dev",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      title: "RestSharp",
+      logo: {
+        alt: "RestSharp Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
+        },
+        {
+          href: 'https://github.com/RestSharp/RestSharp',
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/restsharp",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/RestSharp/RestSharp",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} .NET Foundation. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: themes.vsLight,
+      darkTheme: themes.vsDark,
+      additionalLanguages: ['csharp'],
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
